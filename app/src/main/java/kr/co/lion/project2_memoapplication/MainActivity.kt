5e77aaaba76b974fd02cb21fc0262b1994fd7ab2
zunione <xt.wish1@gmail.com>
2024-02-09 16:54:08 +0900

    Feat: 첫번째 화면

--- app/src/main/java/kr/co/lion/project2_memoapplication/MainActivity.kt
+++ app/src/main/java/kr/co/lion/project2_memoapplication/MainActivity.kt
@@ -1,11 +1,104 @@
 package kr.co.lion.project2_memoapplication
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.divider.MaterialDividerItemDecoration
+import kr.co.lion.project2_memoapplication.databinding.ActivityMainBinding
+import kr.co.lion.project2_memoapplication.databinding.RowMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var activityMainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        setToolbar()
+        setRecyclerView()
+    }
+
+    fun setToolbar() {
+        activityMainBinding.apply {
+            toolbarMain.apply {
+                title = "메모 관리"
+                setTitleTextColor(Color.WHITE)
+
+                // menu_main 연결
+                inflateMenu(R.menu.menu_main)
+                setOnMenuItemClickListener {
+                    when(it.itemId) {
+                        // R.id.menu_item_main_add ->
+                    }
+                    true
+                }
+            }
+        }
+    }
+
+    fun setRecyclerView() {
+        activityMainBinding.apply {
+            recyclerViewMain.apply {
+                adapter = RecyclerViewMainAdapter()
+                layoutManager = LinearLayoutManager(this@MainActivity)
+
+                // 구분선
+                val deco = MaterialDividerItemDecoration(this@MainActivity, MaterialDividerItemDecoration.VERTICAL)
+                addItemDecoration(deco)
+            }
+        }
+    }
+
+    inner class RecyclerViewMainAdapter: RecyclerView.Adapter<RecyclerViewMainAdapter.ViewHolderMain>() {
+        inner class ViewHolderMain (rowMainBinding: RowMainBinding) : RecyclerView.ViewHolder(rowMainBinding.root){
+            val rowMainBinding:RowMainBinding
+
+            init {
+                this.rowMainBinding = rowMainBinding
+
+                this.rowMainBinding.root.layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+                )
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderMain {
+            val rowMainBinding = RowMainBinding.inflate(layoutInflater)
+            val viewHolderMain = ViewHolderMain(rowMainBinding)
+
+            return viewHolderMain
+        }
+
+        override fun getItemCount(): Int {
+            return 20
+        }
+
+        override fun onBindViewHolder(holder: ViewHolderMain, position: Int) {
+            holder.rowMainBinding.textViewRowMainTitle.text = "제목 $position"
+            holder.rowMainBinding.textViewRowMainDate.text = "날짜 $position"
+
+
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
