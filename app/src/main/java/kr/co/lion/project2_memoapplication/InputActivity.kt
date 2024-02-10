041cf0c81a4bffd28575ae301d227d62d61ca604
zunione <xt.wish1@gmail.com>
2024-02-10 18:07:46 +0900

    Feat: InputActivity에 툴바 구성

--- app/src/main/java/kr/co/lion/project2_memoapplication/InputActivity.kt
+++ app/src/main/java/kr/co/lion/project2_memoapplication/InputActivity.kt
@@ -1,11 +1,34 @@
 package kr.co.lion.project2_memoapplication
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.lion.project2_memoapplication.databinding.ActivityInputBinding
 
 class InputActivity : AppCompatActivity() {
+    lateinit var activityInputBinding: ActivityInputBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_input)
+
+        activityInputBinding = ActivityInputBinding.inflate(layoutInflater)
+        setContentView(activityInputBinding.root)
+
+        setToolbar()
+    }
+
+    fun setToolbar() {
+        activityInputBinding.apply {
+            toolbarInput.apply {
+                title = "메모 작성"
+                setTitleTextColor(Color.WHITE)
+
+                // 뒤로 가기
+                setNavigationIcon(R.drawable.arrow_back_24px)
+                setNavigationOnClickListener {
+                    setResult(RESULT_CANCELED)
+                    finish()
+                }
+            }
+        }
     }
 }
