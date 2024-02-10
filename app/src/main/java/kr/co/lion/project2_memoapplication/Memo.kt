2b3d8d2dda979c7bf977cfed2aec92bff6907eed
zunione <xt.wish1@gmail.com>
2024-02-10 21:57:28 +0900

    Memo.kt

---
+++ app/src/main/java/kr/co/lion/project2_memoapplication/Memo.kt
@@ -0,0 +1,35 @@
+package kr.co.lion.project2_memoapplication
+
+import android.os.Parcel
+import android.os.Parcelable
+
+class Memo(var title: String?, var text: String?, var date: String?) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(title)
+        parcel.writeString(text)
+        parcel.writeString(date)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Memo> {
+        override fun createFromParcel(parcel: Parcel): Memo {
+            return Memo(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Memo?> {
+            return arrayOfNulls(size)
+        }
+    }
+
+
+}
